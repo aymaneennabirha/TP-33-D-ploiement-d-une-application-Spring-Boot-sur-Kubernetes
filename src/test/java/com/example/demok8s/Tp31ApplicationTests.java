f5452c268dcad73f257cbf19cacc11912fed95db
ennabirha aymane <ennabirhaayman@gmail.com>
2026-01-05 23:15:42 +0100

    tp 33

---
+++ src/test/java/com/example/demok8s/Tp31ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demok8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp31ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
